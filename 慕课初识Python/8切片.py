dbc3cab22e78bd0c164eca3e1dbbe84e7f102d20
zy89 <yzhiyuan0321@gmail.com>
2019-10-23 23:29:46 +1100

    8切片

--- 慕课初识Python/8切片.py
+++ 慕课初识Python/8切片.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*
 
-#对list切片; 对tuple切片操作也一样，输出为tuple
+#1.对list切片; 对tuple切片操作也一样，输出为tuple
 L = ['Adam', 'Lisa', 'Bart', 'Paul']
 
 a = L[0:3]  #从index0开始到index3，不包括3。
@@ -18,3 +18,27 @@ L = range(1, 101)  #range()创建一个数列；(1,2,3....100)
 print L[0:10] #前10个数
 print L[2::3] #3的倍数
 print L[4:50:5] #不大于50的5的倍数
+
+#2.倒序切片
+L = ['Adam', 'Lisa', 'Bart', 'Paul']
+
+L[-2:] #倒2开始到结束：['Bart', 'Paul']
+
+L[:-2] #index0开始到倒2：['Adam', 'Lisa']
+
+L[-3:-1] #倒3开始到倒1，不包括倒1：['Lisa', 'Bart']
+
+L[-4:-1:2] #倒4开始到倒1，每2个取一个：['Adam', 'Bart']
+
+#3.字符串切片，输出仍是字符串。规则都一样
+#输出string首字母大写
+def firstCharUpper(s):
+    a = s[0].upper()+s[1:]
+    return a
+
+print firstCharUpper('hello')
+print firstCharUpper('sunday')
+print firstCharUpper('september')
+    
+
+

245b1b68d061184ad765e332209d120865eb74c1
zy89 <yzhiyuan0321@gmail.com>
2019-10-24 21:15:29 +1100

    9迭代

--- 慕课初识Python/9迭代.py
+++ 慕课初识Python/9迭代.py
@@ -35,3 +35,19 @@ sum = 0.0
 for s in d.values():
     sum = sum+s
 print sum/len(d)
+
+#4.迭代dict的key和value
+'''
+items()跟values()一样用法，只不过是把每对转换成tuple存在list里
+iteritems()不转换，节省内存
+'''
+
+#打印出 name : score，最后再打印出平均分 average : score。
+d = { 'Adam': 95, 'Lisa': 85, 'Bart': 59, 'Paul': 74 }
+
+sum = 0.0
+for k, v in d.items():
+    sum = sum + v
+    print k,':',v
+print 'average', ':', sum/len(d)
+
